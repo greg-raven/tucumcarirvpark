359c808a241bf1e26f35cd299e14dc9e77207c0d
Greg Raven <greg@corax.org>
2018-09-03 14:46:49 -0700

    change local folder name

--- cgi-bin/sitemap_gen.sh
+++ cgi-bin/sitemap_gen.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # invoke with cron job that calls --
-# /Users/greg/Sites/tucumcarirvpark/tucumcarirvpark-build/cgi-bin/sitemap_gen.sh
+# /Users/greg/Sites/blaze-in-saddle/blaze-in-saddle-build/cgi-bin/sitemap_gen.sh
 
 # Regenerate the sitemap
 # Move to the directory that contains the Google sitemap generator
-cd /Users/greg/Sites/tucumcarirvpark/tucumcarirvpark-build/cgi-bin/
+cd /Users/greg/Sites/blaze-in-saddle/blaze-in-saddle-build/cgi-bin/
 echo "Now regenerating sitemap for blaze-in-saddle.com."
-python sitemap_gen.py --config=/Users/greg/Sites/tucumcarirvpark/tucumcarirvpark-build/cgi-bin/blaze-in-saddle_config.xml
+python sitemap_gen.py --config=/Users/greg/Sites/blaze-in-saddle/blaze-in-saddle-build/cgi-bin/blaze-in-saddle_config.xml
